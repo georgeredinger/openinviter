81f393e8056f51fc7fe783994ab2580799fd2edd
@jgoodsen <jgoodsen@radsoft.com>
2011-03-01 16:04:54 -0700

    added apache configs

--- baseServer.sh
+++ baseServer.sh
@@ -14,3 +14,13 @@ apt-get update
 apt-get upgrade
 apt-get -y install php5-dev php5-cli php-pear php5-mcrypt php5-curl php5-xsl php5-mysql php5-sqlite
 apt-get -y install apache2 libapache2-mod-php5
+
+ln -s /etc/apache2/mods-available/rewrite.load /etc/apache2/mods-enabled/rewrite.load
+ln -s /etc/apache2/mods-available/vhost_alias.load /etc/apache2/mods-enabled/vhost_alias.load
+
+cp /var/www/openinviter/brs-api-staging.conf.template /etc/apache2/sites-enables/brs-api-staging.conf
+
+mkdir -p /var/www/logs
+chown -R :www-data /var/www
+
+/etc/init.d/apache2 restart
